7bdcc21a4f2541b0494a462e6acaae3dde8c5597
Manuel <tofuah@gmail.com>
2021-08-25 15:48:43 +0100

    added functioncounter.js file

--- src/component/FunctionCouter.js
+++ src/component/FunctionCouter.js
@@ -1,4 +1,26 @@
-const FunctionCounter = (props)=>{
+import React, { useState } from 'react'
+
+const FunctionCounter = ()=>{
+    const [count, setCount] = useState(0);
+
+    function handleIncrease() {
+        setCount(count + 1);
+    }
+
+    function handleDecrease() {
+        setCount(count - 1);
+    }
+
+
     return(
-        
+        <>
+        <h1 className= "display">{count} </h1>
+        <div>
+            <button onClick={()=> handleDecrease()} className='cont-button'>Decrease Count </button>
+            <button onClick={()=> handleIncrease()} className='cont-button'>Increase Count </button>
+
+        </div>
+        </>
     )
+}
+export default FunctionCounter;
