5a63f85f1552f8ef28656415818709b3d66256b3
Manuel <tofuah@gmail.com>
2021-08-25 15:49:50 +0100

    added app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import './component/ClassCounter'
 import ClassCounter from './component/ClassCounter';
+import  FunctionCounter  from './component/FunctionCouter';
 
 function App() {
   return (
     <div className="App">
       <ClassCounter />
+      <FunctionCounter />
     </div>
   );
 }
